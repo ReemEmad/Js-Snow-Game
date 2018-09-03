d55b75bbc95d8deeaceb87605b5c36b8761f72a5
Abeerzeina <abeer987@gmail.com>
2018-09-03 06:12:38 +0200

    snow

--- index.js
+++ index.js
@@ -25,6 +25,75 @@ function validate()
 
 
 
+var canvas = document.getElementById("can");
+var ctx = canvas.getContext("2d");
+var h = window.innerHeight
+var w  = window.innerWidth;
+canvas.width = w;
+canvas.height = h
+var snowContainer=[];
+var maxSnow = 500;
+
+var angle = 0;
+for(var i=0;i<maxSnow;i++)
+    {
+    var snow = 
+        {x:Math.random()*w , 
+        y:Math.random()*h , 
+         r:Math.random()*10
+        
+        }
+    snowContainer.push(snow)
+
+    }
+function drawSnow()
+{
+    
+    ctx.clearRect(0,0,w,h);
+    ctx.beginPath();
+    for(var i=0;i<snowContainer.length;i++)
+        {
+            
+ctx.moveTo(snowContainer[i].x,snowContainer[i].y)            
+ctx.arc(snowContainer[i].x,snowContainer[i].y,snowContainer[i].r,0,2*Math.PI);
+            
+        }
+
+ctx.fillStyle="white";
+ctx.fill();    
+
+moveSnow()
+}
+
+
+function moveSnow()
+{
+    angle +=0.01;
+    for(var i=0;i<snowContainer.length;i++)
+        {
+            
+        var s = snowContainer[i];
+            s.y += Math.pow(1,2)+1;
+                s.x +=Math.sin(angle)*2;
+                            
+    if(s.y>h)
+        {
+            snowContainer[i]=
+            {
+            x:s.x,
+            y:0,
+            r:s.r
+            }
+        }
+
+        }
+}
+
+setInterval(drawSnow,20)
+
+
+
+
 
 
 
